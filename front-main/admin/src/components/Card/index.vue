d6085d4abfa6949e1e3730bc237155dbf5dae089
符南山 <2414785575@qq.com>
2025-03-04 20:25:10 +0800

    新增自定义卡片组件

---
+++ front-main/admin/src/components/Card/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="module">
+    <div class="title">{{ title }}</div>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
